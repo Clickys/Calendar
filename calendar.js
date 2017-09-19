e333c34617cb3da08591c8a429e2b3b847e161e3
clickys <koc.clicky@gmail.com>
2017-09-19 23:32:52 +0300

    Fix the current day to set background as white

--- calendar.js
+++ calendar.js
@@ -24,25 +24,53 @@ var calendar = {
 
 
 var loopTable = function () {
-    var k = getDaysInMonth(calendar.year(), calendar.month());
+    var daysInMonth = getDaysInMonth(calendar.year(), calendar.month());
     var table = document.getElementById('myTable');
     var rows = table.rows;
     var l = 1;
+    var month = calendar.month();
+    var year = calendar.year();
+    var firstDay = new Date(year + "-" + month).getDay();
+    var currentDay = new Date().getDay();
+    var dayOfMonth = new Date().getDate();
 
+    console.log(dayOfMonth);
     for (var i = 1; i < rows.length; i++) {
-        cells = rows[i].cells;
-        for (var j = 0; j < cells.length; j++) {
-            if (l <= k) {
-                cells[j].innerHTML = l;
-            } else {
-                break;
+
+        if (rows[i] == rows[1]) {
+
+            var k = 1;
+
+            for (var j = firstDay; j < rows[i].cells.length; j++) {
+
+                if (k === dayOfMonth) {
+                    rows[i].cells[j].style.backgroundColor = 'white'
+                    console.log('iam in style');
+                    console.log(rows[i].cells[j])
+
+                }
+
+                if (k <= daysInMonth) {
+                    console.log('iam in k <')
+                    rows[i].cells[j].innerHTML = k;
+                    k++
+                }
+
             }
-
-            l++
-
+        } else {
+            for (let j = 0; j < rows[i].cells.length; j++) {
+                if (k === dayOfMonth) {
+                    rows[i].cells[j].style.backgroundColor = 'white'
+                }
+                if (k <= daysInMonth) {
+                    rows[i].cells[j].innerHTML = k;
+                    k++
+                }
+            }
         }
     }
 }
+
 loopTable();
 
 function monthTitle() {
@@ -53,12 +81,11 @@ function monthTitle() {
     var title = document.getElementById('calendarTitle');
     var nextArrow = document.getElementById('nxt');
     var leftArrow = document.getElementById('prev');
-    
-    
+
+
     if (monthNum >= 12) {
         nextArrow.className += ' inactiveLink';
-    } 
-    else if (monthNum <= 1) {
+    } else if (monthNum <= 1) {
         leftArrow.className += ' inactiveLink';
     } else {
         nextArrow.classList.remove('inactiveLink');
@@ -68,8 +95,7 @@ function monthTitle() {
     title.innerHTML = '';
     var titleNode = document.createTextNode(monthName);
     title.appendChild(titleNode);
-    
-    console.log(calendar.month());
+
 
 }
 monthTitle();
